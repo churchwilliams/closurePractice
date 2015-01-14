fb23e93f8767134a5e306a520650cb05f47935d0
Eric Williams <eric@churchwilliams.com>
2015-01-14 12:27:29 -0700

    finshed all but last one. review all again

--- practice.js
+++ practice.js
@@ -9,11 +9,11 @@ var outer = function(){
 //Above you're given a function that returns another function which has a closure over the name variable.
 //Invoke outer saving the return value into another variable called 'inner'.
 
-  //Code Here
+var inner = outer();
 
 //Once you do that, invoke inner.
 
-  //Code Here
+inner();
 
 
 
@@ -32,7 +32,8 @@ var callFriend = function(){
 //Above you're given a callFriend function that returns another function.
 //Do what you need to do in order to call your function and get 'Calling Jake at 435-215-9248' in your console.
 
-  //Code Here
+var inner = callFriend();
+inner(4352159248);
 
 
 
@@ -44,12 +45,19 @@ var callFriend = function(){
   Write a function called makeCounter that makes the following code work properly.
 */
 
-  //Code Here
+var makeCounter = function() {
+  counter = 0;
+  return function() {
+    counter += 1;
+    return counter;
+  };
+};
+
   var count = makeCounter();
-  count() // 1
-  count() // 2
-  count() // 3
-  count() // 4
+  count(); // 1
+  count(); // 2
+  count(); // 3
+  count(); // 4
 
 
 
@@ -60,9 +68,21 @@ var callFriend = function(){
 /*
   Write a function that accepts another function as it's only argument and returns a new function
   (which invokes the original function that was passed in) that can only ever be executed once.
-  Once completed, add a second arguments that allows the function to be invoked N number of times.
+
+  Once completed, add a second argument that allows the function to be invoked N number of times.
   After the function has been called N number of times, console.log('STAHHP');
 */
+
+var fn1 = function(fn2) {
+  return function() {
+    return fn1();
+  }
+};
+
+var secondArg = function(arg2)
+
+
+
 
 
 
